674bacacc54a51e946671b9faf0ff79b1d76de83
Isaías Vela <isaiasvelasotil@gmail.com>
2026-07-29 13:14:23 +0200

    Ci: Add GitHub Actions CI pipeline (#4)
    * Feat: add ci workflow
    
    * Ci: Run workflow on all pushes
    
    * Format: Apply Terraform formatting
    
    * Ci: Add TFLint to Terraform workflow
    
    * Ci: Add Trivy and Checkov to Terraform Workflow
    
    * Ci: Add workflow permissions and Checkov exceptions
    
    * Ci: Sckip CKV_GIT_1 and CKV_GIT_5 checks
    
    * Ci: Comments about CKV_GIT_1 and CKV_GIT_5

--- terraform/files.tf
+++ terraform/files.tf
@@ -1,14 +1,14 @@
 resource "github_repository_file" "README" {
-    repository = github_repository.repository.name
-    file = "README.md"
-    content = file("../templates/README.md")
-    commit_message = "Add README.md file"
-    overwrite_on_create = true
+  repository          = github_repository.repository.name
+  file                = "README.md"
+  content             = file("../templates/README.md")
+  commit_message      = "Add README.md file"
+  overwrite_on_create = true
 }
 
 resource "github_repository_file" "LICENSE" {
-    repository = github_repository.repository.name
-    file = "LICENSE"
-    content = file("../templates/LICENSE")
-    commit_message = "Add LICENSE file"
+  repository     = github_repository.repository.name
+  file           = "LICENSE"
+  content        = file("../templates/LICENSE")
+  commit_message = "Add LICENSE file"
 }
--- terraform/labels.tf
+++ terraform/labels.tf
@@ -2,47 +2,47 @@ resource "github_issue_labels" "labels" {
   repository = github_repository.repository.name
 
   label {
-    name = "bug"
+    name  = "bug"
     color = "d73a4a"
   }
 
   label {
-    name = "documentation"
+    name  = "documentation"
     color = "0075ca"
   }
 
   label {
-    name = "enhancement"
+    name  = "enhancement"
     color = "a2eeef"
   }
 
   label {
-    name = "feature"
+    name  = "feature"
     color = "0e8a16"
   }
 
   label {
-    name = "security"
+    name  = "security"
     color = "e11d21"
   }
 
   label {
-    name = "dependencies"
+    name  = "dependencies"
     color = "cfd3d7"
   }
 
   label {
-    name = "devops"
+    name  = "devops"
     color = "5319e7"
   }
 
   label {
-    name = "good first issue"
+    name  = "good first issue"
     color = "7057ff"
   }
 
   label {
-    name = "help wanted"
+    name  = "help wanted"
     color = "008672"
   }
 }
